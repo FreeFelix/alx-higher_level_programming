f2f6af4e4de0abb4d82c572015a56f3321f58529
FreeFelix <nijeanfelix@gmail.com>
2023-10-02 17:24:34 -0700

    Add another files

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -4,23 +4,22 @@
 #include <stdlib.h>
 
 /**
- * struct listintiger_s - single linked list structure.
- * @i: passed argunment int
- * @next: node pointed to the next node
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
  *
- * Description: Single linked with node and structure for
- * Alx high level programming project.
+ * Description: singly linked list node structure
+ * 
  */
-typedef struct listintiger_s
+typedef struct listint_s
 {
-	int i;
-	struct listintiger_s *next;
-} listintiger_t;
+    int n;
+    struct listint_s *next;
+} listint_t;
 
-size_t print_listintiger(const listintiger_t *h);
-listintiger_t *add_nodeint(listintiger_t **head, const int i);
-void free_listintiger(listintiger_t *head);
-int check_cycle(listintiger_t *list);
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
 
-#endif
-
+#endif /* LISTS_H */
