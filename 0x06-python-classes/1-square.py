99a42ac2760f6559bff50997c7f1d7acb0c99c31
FreeFelix <nijeanfelix@gmail.com>
2023-10-25 15:46:04 -0700

    new

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""Defines a Rectangle class."""
+"""
+    Defines a Rectangle class.
+"""
 
 
 class Rectangle:
-    """Represent a rectangle."""
+    """
+        Represent a rectangle.
+    """
 
     def __init__(self, width=0, height=0):
-        """Initialize a new Rectangle.
+        """
+            Initialize a new Rectangle.
 
         Args:
             width (int): The width of the new rectangle.
@@ -17,7 +22,9 @@ class Rectangle:
 
     @property
     def width(self):
-        """Get/set the width of the rectangle."""
+        """
+            Get/set the width of the rectangle.
+        """
         return self.__width
 
     @width.setter
@@ -30,7 +37,9 @@ class Rectangle:
 
     @property
     def height(self):
-        """Get/set the height of the rectangle."""
+        """
+            Get/set the height of the rectangle.
+        """
         return self.__height
 
     @height.setter
